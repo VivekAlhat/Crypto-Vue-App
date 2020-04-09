2b07d321443afcdf8fc6f98edededbbd6819647d
VivekAlhat <vivekalhat14@outlook.com>
2020-04-09 23:42:43 +0530

    Final Build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="hello">
+  <div class="title-main">
+    <h2>Cryptocurrency Price Indicator</h2>
   </div>
+  <div id="crypto-container" v-for="(value, key) in cryptos"  :key="value.key">
+    <span class="left">{{ key }}</span>
+    <span class="right">₹ {{ value.INR }}</span>
+  </div>
+</div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'hello',
+  data: () => ({
+    cryptos: [],
+    errors: []
+  }),
+  created () {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,BSV,BCH,BNB,XMR,LINK,PAX,EOS,ETC,LTC&tsyms=INR')
+      .then(response => {
+        this.cryptos = response.data
+        console.log(response)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+body {
+  background: #f1f1f1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+h2 {
+  color: black;
+  padding-bottom: 10px;
+  font-family: monospace;
+  font-size: 30px;
+  letter-spacing: 2px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+div#crypto-container {
+  background: white;
+  width: 50%;
+  height: 65px;
+  margin: 0 auto 4px auto;
+  padding: 1em;
+  box-shadow: 1px 1px 0 lightgrey;
 }
-a {
-  color: #42b983;
+
+span.left {
+  padding-top: 5px;
+  text-align: center;
+  float: left;
+  font-weight: bold;
+}
+
+span.right {
+  padding-top: 5px;
+  float: right;
 }
 </style>
